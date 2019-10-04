ae02df6b8293b7763606b6ef3f05f30aafd90dc1
sabina929 <sabina.abbasova2909@gmail.com>
2019-10-04 21:40:00 +0400

    first commit

---
+++ src/components/Button.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+text-transform: capitalize;
+font-size: 1.2rem;
+color: var(--lightGray);
+background: var(--mainRed);
+// border: 3px solid var(--mainRed);
+border: none;
+border-radius: 5px;
+padding: .3rem .6rem;
+cursor: pointer;
+margin: .2rem .5rem .2rem 0;
+transition: all .4s ease;
+&:hover {
+    background: var(--mainBlue);
+} 
+&:focus {
+    background: var(--mainBlue);
+    outline: none;
+} 
+`;
